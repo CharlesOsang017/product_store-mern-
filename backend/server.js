5d18c1571000e6d7b2760293bb5490c879dc41ce
CharlesOsang017 <charlesosango02@gmail.com>
2024-10-01 21:45:38 +0300

    Add deployment scripts

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/products", productRoutes);
 // }
 
 //creating paths for deployment
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
