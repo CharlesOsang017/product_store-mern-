d8fcc91eb4574a3d6420bc3b8e3aa790b15698e0
CharlesOsang017 <charlesosango02@gmail.com>
2024-10-01 21:10:25 +0300

    Add deployment scripts

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectToDB } from "./config/db.js";
-import path from "path";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -16,13 +16,19 @@ app.use(express.json()); // allows JSON data to be accepted in the req.body
 app.use("/api/products", productRoutes);
 
 // deployment setup
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
+
+//creating paths for deployment
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(port, () => {
   connectToDB();
